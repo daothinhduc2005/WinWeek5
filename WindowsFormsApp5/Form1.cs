8316a71540923688566eff522fcee9cc2852b338
l1quan268 <tranhongquan26082005@gmail.com>
2025-05-20 22:08:26 +0700

    bai 1 dep keng

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp5
@@ -15,6 +10,39 @@ namespace WindowsFormsApp5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SmtpClient smtpClient = new SmtpClient("127.0.0.1", 25)) // Hoặc 587 nếu bạn đã đổi cổng
+            {
+                string mailfrom = textBox1.Text.Trim();
+                string mailto = textBox2.Text.Trim();
+                string password = textBox3.Text.Trim();
+
+                smtpClient.EnableSsl = false; // Nếu bạn bật SSL trong MDaemon thì đổi thành true
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(mailfrom, password);
+
+                using (MailMessage message = new MailMessage())
+                {
+                    try
+                    {
+                        message.From = new MailAddress(mailfrom);
+                        message.To.Add(mailto);
+                        message.Subject = textBox4.Text.Trim();
+                        message.Body = richTextBox1.Text;
+                        message.IsBodyHtml = true;
+
+                        smtpClient.Send(message);
+                        MessageBox.Show("Gửi mail thành công!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi gửi mail: " + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
